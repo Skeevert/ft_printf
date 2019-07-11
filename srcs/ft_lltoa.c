4e11e802bc2dca2dfc232cf3e307b350b1cf13f4
Stackspear Vivienne <svivienn@at-j6.21-school.ru>
2019-07-11 16:45:40 +0300

    добавил itoa для int64

---
+++ srcs/ft_lltoa.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lltoa.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svivienn <svivienn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 15:47:23 by svivienn          #+#    #+#             */
+/*   Updated: 2019/07/11 16:44:28 by svivienn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+char *ft_itoa64(int64_t c)
+{
+    int     size;
+    int64_t save;
+    char    *str;
+    
+    size = 0;
+    save = c;
+    while(save != 0)
+    {
+        size++;
+        save = save / 10;
+    }
+    if (c < 0)
+        size++;
+    str = (char*)malloc(sizeof(char) *  (size + 1));
+    str[size] = '\0';
+    if (c < 0)
+        str[0] = '-';
+    while (c != 0)
+    {
+        str[size - 1] = (c % 10) + '0';
+        size--;
+        c = c / 10;
+    }
+    return (str);
+}
+
+char *ft_itoa64(uint64_t c)
+{
+    int     size;
+    uint64_t save;
+    char    *str;
+    
+    size = 0;
+    save = c;
+    while(save != 0)
+    {
+        size++;
+        save = save / 10;
+    }
+    str = (char*)malloc(sizeof(char) *  (size + 1));
+    str[size] = '\0';
+    while (c != 0)
+    {
+        str[size - 1] = (c % 10) + '0';
+        size--;
+        c = c / 10;
+    }
+    return (str);
+}
